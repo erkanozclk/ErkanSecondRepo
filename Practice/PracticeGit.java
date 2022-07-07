137dc2f813bbfcaa665843b167a0d06e55da378e
Erkan Ozcelik <erkanozcelik19@gmail.com>
2022-07-07 11:20:01 -0400

    TEst1 Test

---
+++ Practice/PracticeGit.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class PracticeGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Testttttt");
+    }
+}
---
+++ test1branch/test1.java
@@ -0,0 +1,4 @@
+package test1branch;
+
+public class test1 {
+}

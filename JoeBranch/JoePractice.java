24c784678007ddd05901c2cf610cc4633a04f413
Erkan Ozcelik <erkanozcelik19@gmail.com>
2022-07-07 19:34:04 -0400

    This is my first branching practice bla bla

---
+++ JoeBranch/JoePractice.java
@@ -0,0 +1,7 @@
+package JoeBranch;
+
+public class JoePractice {
+    public static void main(String[] args) {
+        System.out.println("Heyyyy this is Joe Branch, I just checkout it");
+    }
+}

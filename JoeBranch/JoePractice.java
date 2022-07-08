e61ef15c0624005a04480fb4ae7a5d506afc2fd3
ErkanOzclk <68093393+erkanozclk@users.noreply.github.com>
2022-07-07 20:55:15 -0400

    Update JoePractice.java

--- JoeBranch/JoePractice.java
+++ JoeBranch/JoePractice.java
@@ -3,5 +3,7 @@ package JoeBranch;
 public class JoePractice {
     public static void main(String[] args) {
         System.out.println("This is updated file");
+        
+         System.out.println("This is updated file 2");
     }
 }
